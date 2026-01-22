160acc08816c514c74604d423fea2d4489b8e0fe
PhilTBatt <philtbatt@gmail.com>
2026-01-22 19:30:59 +0000

    add svgs and arrows to diagram

--- src/app/components/system-architecture/system-architecture.ts
+++ src/app/components/system-architecture/system-architecture.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { StyledCard } from '../styled-card/styled-card';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-system-architecture',
-  imports: [StyledCard],
+  imports: [StyledCard, NgClass],
   templateUrl: './system-architecture.html',
   styleUrl: './system-architecture.scss'
 })
 
 export class SystemArchitecture {
   steps = [
-    'ANGULAR FRONTEND',
-    'JAVA API',
-    'AWS S3',
-    'PYTHON LAMBDA',
-    'DYNAMODB',
-    'API & UI UPDATE'
-  ]
+    { key: 'angular', label: 'ANGULAR FRONTEND', icon: 'assets/diagram/angular.svg' },
+    { key: 'java',    label: 'JAVA API',         icon: 'assets/diagram/java.svg' },
+    { key: 's3',      label: 'AWS S3',           icon: 'assets/diagram/s3.svg' },
+    { key: 'lambda',  label: 'PYTHON LAMBDA',    icon: 'assets/diagram/lambda.svg' },
+    { key: 'ddb',     label: 'DYNAMODB',         icon: 'assets/diagram/dynamodb.svg' },
+    { key: 'update',  label: 'API & UI UPDATE',  icon: 'assets/diagram/update.svg' },
+    ]
 }
